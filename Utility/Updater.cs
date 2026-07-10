b1c64af553cbe5aa2bf0e730135ed627ce1f606b
Zaql-Infin <zaqlwrld@gmail.com>
2026-07-10 01:09:24 -0700

    v71: overlay top-left, dark bg matching limiter, always click-through
    - OverlayWindow: reposition from bottom-left to top-left of game window
    - OverlayWindow: panel background #CC111111 -> #E01c1c22 (limiter dark navy)
    - OverlayWindow: WS_EX_TRANSPARENT always applied (never intercepts mouse)
    - Updater: bump to v71.0.0

--- Utility/Updater.cs
+++ Utility/Updater.cs
@@ -11,8 +11,8 @@ namespace InfinLimit.Utility
 {
     public static class Updater
     {
-        public const int    Version       = 70;
-        public const string VersionString = "70.0.0";
+        public const int    Version       = 71;
+        public const string VersionString = "71.0.0";
 
         public const string RepoOwner = "Zaql-Infin";
         public const string RepoName  = "infinlimit";
